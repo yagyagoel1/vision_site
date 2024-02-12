f9f907dcebe0475e2c2202ff4660892a1b333ef7
yagya <yagyagoel87@gmail.com>
2024-02-12 15:18:33 +0530

    added the navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
   
 
   return (
     <>
-     
+     <Navbar></Navbar>
+     <Home></Home>
     </>
   )
 }
