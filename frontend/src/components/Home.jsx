66af035c3dee45b0a49cbdd13c0e73fa105391d7
yagya <yagyagoel87@gmail.com>
2024-02-12 15:58:49 +0530

    modified the tailwind config file to get the logo right

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='  bg-[#010001] h-screen max-w'>
+        <div className='text-6xl sm:text-7xl md:text-8xl  xl:text-9xl flex items-center justify-center tracking-wide sm:tracking-wide md:tracking-wider xl:tracking-widest h-screen text-white font-montserrat font-bold'>
+            THE VISION
+        </div>
+    </div>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      '9xl' : ['187px','1315px' ],
+      '8xl' : ['120px','1000px' ],
+      '7xl' : ['105px','900px' ],
+      '6xl' : ['80px','900px' ]
+    },
     extend: {},
   },
   plugins: [],
