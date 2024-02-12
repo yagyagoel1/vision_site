60700f51958dfdd40be3c3728d1219ef26d564e6
yagya <yagyagoel87@gmail.com>
2024-02-13 00:09:12 +0530

    made some changes with event

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -22,7 +22,7 @@ const Event = () => {
 
 
 
-        <div className=' lg:px-15 xl:px-20 md:px-10 px-5 flex flex-col justify-start items-center sm:grid grid-cols-3 gap-2 '>
+        <div className=' py-8 lg:px-15 xl:px-20 md:px-10 px-5 flex sm:flex-row-reverse flex-col-reverse justify-end items-center sm:grid grid-cols-3 gap-2 '>
           <div className=' col-span-1 px-5'>
             <div className='font-bold text-5xl xl:text-6xl  lg:text-5xl md:text-4xl sm:text-3xl'>BUILD-IT</div>
             <div className=' italic text-[#777676] px-5 lg:px-5 xl:px-9 sm:px-3 sm:text-xl lg:text-3xl text-2xl md:text-2xl'>"Hackathon"</div>
