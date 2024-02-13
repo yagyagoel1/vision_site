c946d0e19c2b9e7db40c2a36b5a2e64928b79ec5
yagya <yagyagoel87@gmail.com>
2024-02-13 21:00:45 +0530

    added the open category thingy

---
+++ frontend/src/components/OpenCat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OpenCat = () => {
+  return (
+    <div className='w-full h-auto bg-[#010001] flex justify-center items-center'>
+        <div className='bg-[#d9d9d9] rounded-[25px] italic font-bold py-2 px-16 lg:text-6xl md:text-5xl text-3xl'>#10 OPEN CATETORY</div>
+    </div>
+  )
+}
+
+export default OpenCat
