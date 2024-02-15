77444ea800f88ded769264fe0c03a0024aaeae10
Yagya Goel <149003369+yagyagoel1@users.noreply.github.com>
2024-02-16 05:06:59 +0530

    added the react smooth scroll

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className='bg-[#010001] max-w  h-screen '>
+        <div name = "home" className='bg-[#010001] max-w  h-screen '>
             <div className='  flex items-center justify-center flex-col pt-[300px] max-w'>
                 <div className='text-6xl sm:text-7xl md:text-8xl  xl:text-9xl  tracking-wide sm:tracking-wide md:tracking-wider xl:tracking-widest text-white font-montserrat font-bold'>
                     THE VISION
