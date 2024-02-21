8a6b5a2c4a0bb775c5a26244d63b5b4b0c905d9b
yagya <yagyagoel87@gmail.com>
2024-02-22 03:27:27 +0530

    added the innovate section

---
+++ frontend/src/components/Innovate.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Innovate = () => {
+    return (
+        <div className='w-full h-auto bg-[#010001] text-white'>
+            {/*Container*/}
+            <div className='flex flex-col items-center pt-16'>
+                <div className="font-bold xl:text-7xl sm:text-6xl text-5xl">I N N O V A T E</div>
+                <div className='text-[#777676]  tracking-widest xl:text-5xl sm:text-3xl text-2xl pb-10'>“ Bring in the best of the best ”</div>
+
+                <div className='flex flex-col items-center'>
+                <div className='flex lg:gap-11 gap-7'>
+                    <div className='flex flex-col lg:gap-11 gap-7'>
+                        <div className='bg-[#d9d9d9] lg:h-[255px] sm:h-[145px] h-[120px] lg:w-[938px] sm:w-[469px] w-[330px]'></div>
+                        <div className='flex items-center '>
+                            <div className='bg-[#d9d9d9] lg:w-[571px] sm:w-[285px] w-[180px] lg:h-[255px] sm:h-[145px] h-[120px] '></div>
+                            <div className='lg:pl-16 pl-7 font-light lg:text-4xl sm:text-3xl text-2xl place-items-center tracking-widest'>PITCH  IT</div>
+                        </div>
+                    </div>
+                    <div  className='bg-[#d9d9d9] lg:w-[255px] sm:w-[145px] w-[120px] lg:h-[555px] sm:h-[320px] h-[268px]'></div>
+                </div>
+                <div className='flex lg:gap-11 gap-7 lg:pt-11 py-7'>
+                    <div className=' bg-[#d9d9d9] lg:w-[275px] sm:w-[160px] w-[120px] lg:h-[255px] sm:h-[145px] h-[120px]'></div>
+                    <div className=' bg-[#d9d9d9] lg:w-[918px] sm:w-[449px] w-[330px] lg:h-[255px]  sm:h-[145px] h-[120px]'></div>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Innovate
